a8ac55e44cf4abb2e8b9f3ed9d489a3074245cd8
nadavsudri <hpcity123@gmail.com>
2024-11-29 18:56:54 +0200

    1.1 Necessary Classes has been added to the project. 1.2 function isnumber created along with some side functions
    1.3 added function number2int
    
    1.4 added function maxIndex, and added some relevant tests.
    
    1.5 Main updated to run like the Jar, added side functions to keep the main organized

--- Ex1/Ex1Main.java
+++ Ex1/Ex1Main.java
@@ -1,24 +1,76 @@
 package Ex1;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Ex1Main {
+    public static void num_show(String num)// prints the number according to its validity
+    {
+        System.out.println("Number = " + num + "| is valid?: " +Ex1.isNumber(num)+"| Value: "+Ex1.number2Int(num));
+        if (!Ex1.isNumber(num)){
+            System.out.println("Err: Not a valid number: "+num);
+        }
+        System.out.println();
+    }
+    public static String get_First() // Gets from user the first number
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number in a valid format (Type 'quit' to exit)': -> ");
+        String num = sc.nextLine();
+        System.out.println();
+        return num;
+    }
+    public static String get_Second() // Gets from user the second number
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter second number in a valid format (Type 'quit' to exit)': -> ");
+        String num= sc.nextLine();
+        System.out.println();
+        return num;
+    }
+    public static void print_arr(String[]arr){
+        System.out.print("[");
+        boolean first = true;
+        for (String s : arr){
+            if (first){System.out.print(s);}
+            else{System.out.print(","+s);}
+            first = false;
+        }
+        System.out.println("]");
+    } //prints array in format of [x,y,z,w]
     public static void main(String[] args) {
-        /*System.out.print("Enter a number in a valid format (Type 'quit' to exit)': -> ");
-        Scanner sc = new Scanner(System.in);
-        String n = sc.next();
-        if (n.equals("quit")){System.exit(0);}
-        while (!Ex1.isNumber(n))
-        {
-            System.out.print("Not a valid number, please try again: -> ");
-            n = sc.next();
-        }*/
-        Scanner sc = new Scanner(System.in);
-       String[] nums = {"19999999bA","1168bA","2222b4","234BbD","637b9","33a34"};
-       for (String num : nums) {
-           System.out.println(Ex1.number2Int(num));
-       }
-        System.out.println(Ex1.maxIndex(nums));
+        while ((true)) // Goes on until user type 'quit'
+        {   String first = get_First();
+            if (first.equals("quit")){System.exit(0);}
+            num_show(first);
+            while(!Ex1.isNumber(first)){
+                first = get_First();
+                if (first.equals("quit")){System.exit(0);}
+                num_show(first);
+            }
+            String second = get_Second();
+            if (second.equals("quit")){System.exit(0);}
+            num_show(second);
+            while(!Ex1.isNumber(second)){
+                second = get_Second();
+                if (second.equals("quit")){System.exit(0);}
+                num_show(second);
+            }
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a base to calculate (Range [2,16]): -> ");
+            int base = sc.nextInt();
+            System.out.println();
+            int sum = (Ex1.number2Int(first)+Ex1.number2Int(second));
+            int mult = (Ex1.number2Int(first)*Ex1.number2Int(second));
+            System.out.println(first+" + "+ second +" = " + Ex1.int2Number(sum,base));
+            System.out.println(first+ " * " + second + " = " + Ex1.int2Number(mult,base));
+            System.out.println();
+            String[] nums = {first,second,Ex1.int2Number(sum,base),Ex1.int2Number(mult,base)};
+            System.out.print("Max number From ");
+            print_arr(nums);
+            System.out.print("is: " + nums[Ex1.maxIndex(nums)]);
+            System.out.println();
+            System.out.println("_______________The_End_____________");
 
 
-
+        }
     }
 }
